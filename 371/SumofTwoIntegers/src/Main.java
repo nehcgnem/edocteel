9cba3cf93896a949bcdc97330c0e898031c93455
nehcgnem <35271275+nehcgnem@users.noreply.github.com>
2019-07-16 02:39:13 -0400

    Leetcode 371 with subtraction

---
+++ 371/SumofTwoIntegers/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution a = new Solution();
+
+        System.out.println(a.getSum(2,3));
+        System.out.println(a.getSub(2,3));
+
+    }
+}
